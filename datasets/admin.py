e395d29a47cfa619b07997a297ef9db8e944f9a7
Dave Bridges <dave.bridges@gmail.com>
2009-04-11 00:16:03 +0000

    added sgd_interaction dataset

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from experimentdb.datasets.models import SGD_phenotypes
+from experimentdb.datasets.models import SGD_phenotypes, SGD_interactions
 
 class SGD_phenotypesAdmin(admin.ModelAdmin):
 	search_fields = ['Phenotype', 'Gene_Name', 'Chemical', 'Condition']
 	list_display = ('Gene_Name', 'Phenotype', 'Chemical', 'Condition')
 admin.site.register(SGD_phenotypes, SGD_phenotypesAdmin)
+
+class SGD_interactionsAdmin(admin.ModelAdmin):
+	search_fields = ['Standard_Gene_Name_Bait', 'Feature_Name_Bait', 'Standard_Gene_Name_Hit', 'Feature_Name_Hit']
+	list_display = ('Standard_Gene_Name_Bait', 'Standard_Gene_Name_Hit', 'Experiment_Type', 'Genetic_or_Physical_Interaction', 'Manually_Curated_or_High_Throughput')
+	list_filter = ('Genetic_or_Physical_Interaction', 'Manually_Curated_or_High_Throughput', 'Experiment_Type')
+admin.site.register(SGD_interactions, SGD_interactionsAdmin)
 
--- datasets/models.py
+++ datasets/models.py
@@ -16,6 +16,23 @@ class SGD_phenotypes(models.Model):
 	Chemical  = models.CharField(max_length=150, blank=True, help_text="Any chemicals relevant to the phenotype")
 	Condition  = models.CharField(max_length=250, help_text="Condition under which the phenotype was observed") 
 	Details  = models.CharField(max_length=250, blank=True, help_text="Details about the phenotype")
-	Reporter  = models.CharField(max_length=250, blank=True, help_text="The protein(s) or RNA(s) used in an experiment to track a process ")
+	Reporter  = models.CharField(max_length=250, blank=True, help_text="The protein(s) or RNA(s) used in an experiment to track a process")
 	def __unicode__(self):
 		return u'%s, %s' % (self.Gene_Name, self.Phenotype)
+
+class SGD_interactions(models.Model):
+	Feature_Name_Bait = models.CharField(max_length=50, help_text="The feature name of the gene used as the bait")
+	Standard_Gene_Name_Bait = models.CharField(max_length=50, help_text="The standard gene name of the gene used as the bait", blank=True)
+	Feature_Name_Hit = models.CharField(max_length=50, help_text="The feature name of the gene that interacts with the bait")
+	Standard_Gene_Name_Hit = models.CharField(max_length=50, help_text="The standard gene name of the gene that interacts with the bait", blank=True)
+	Experiment_Type = models.CharField(max_length=50, help_text="A description of the experimental used to identify the interaction")
+	Genetic_or_Physical_Interaction = models.CharField(max_length=100, help_text="Indicates whether the experimental method is a genetic or physical interaction")
+	Source = models.CharField(max_length=50, help_text="Lists the database source for the interaction")
+	Manually_Curated_or_High_Throughput = models.CharField(max_length=50, help_text="Lists whether the interaction was manually curated from a publication or added as part of a high-throughput dataset")
+	Notes = models.TextField(max_length=250, help_text="Free text field that contains additional information about the interaction", blank=True)
+	Phenotype = models.CharField(max_length=50, help_text="Contains the phenotype of the interaction", blank=True)
+	Reference = models.CharField(max_length=100, help_text="Lists the identifiers for the reference as an SGDID (SGD_REF:) or a PubMed ID (PMID:)")
+	Citation = models.TextField(max_length=500, help_text = "Lists the citation for the reference")
+	def __unicode__(self):
+		return u'%s with %s' % (self.Feature_Name_Bait, self.Feature_Name_Hit)
+
