808cebefbffd1cc4f980bad268c535580355642e
Dave Bridges <dave.bridges@gmail.com>
2009-07-04 15:19:54 +0000

    modified protein app, and adjusted database accordingly

--- proteins/admin.py
+++ proteins/admin.py
@@ -1,7 +1,19 @@
-from experimentdb.proteins.models import Protein
+from experimentdb.proteins.models import ProteinFamily, Protein, Species, ProteinDetail
 from django.contrib import admin
 
+class ProteinFamilyAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(ProteinFamily, ProteinFamilyAdmin)
+
 class ProteinAdmin(admin.ModelAdmin):
-	list_display = ('name', 'gene', 'RefSeqProtein', 'RefSeqNucleotide')
+	list_display = ('name',)
 admin.site.register(Protein, ProteinAdmin)
 
+class SpeciesAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(Species, SpeciesAdmin)
+
+class ProteinDetailAdmin(admin.ModelAdmin):
+	list_display = ('name', 'protein', 'gene', 'RefSeqProtein', 'RefSeqNucleotide')
+	list_filter = ('protein', )
+admin.site.register(ProteinDetail, ProteinDetailAdmin)
--- proteins/models.py
+++ proteins/models.py
@@ -1,17 +1,45 @@
 from django.db import models
 
+class ProteinFamily(models.Model):
+	name = models.CharField(max_length=50)
+	notes = models.TextField(max_length=500, blank=True)
+	def __unicode__(self):
+		return '%s' % self.name
+	class Meta:
+		ordering = ['name']
+
 class Protein(models.Model):
-	name = models.CharField(max_length=10)
-	gene = models.CharField(max_length=10, blank=True)
-	RefSeqProtein = models.CharField(max_length=10, blank=True)
-	RefSeqNucleotide = models.CharField(max_length=10, blank=True)
-	public = models.BooleanField()
-	published = models.BooleanField()
+	protein_family = models.ManyToManyField('ProteinFamily')
+	name = models.CharField(max_length=25)
 	def __unicode__(self):
 		return u'%s' % self.name
 	def get_absolute_url(self):
 		return "/protein/%i/" % self.id
 	class Meta:
 		ordering = ['name']
-
+		
+class Species(models.Model):
+	common_name = models.CharField(max_length=50)
+	scientific_name = models.CharField(max_length=50, blank=True)
+	taxonomy_id = models.IntegerField(blank=True)
+	def __unicode__(self):
+		return '%s' % self.common_name
+		
+class ProteinDetail(models.Model):
+	name = models.CharField(max_length=50)
+	protein = models.ForeignKey('Protein')
+	gene = models.CharField(max_length=10, blank=True)
+	species = models.ForeignKey('Species')
+	geneID = models.IntegerField(blank=True)
+	RefSeqProtein = models.CharField(max_length=15, blank=True, verbose_name="Protein Sequence (RefSeq)")
+	RefSeqProtein_gi = models.IntegerField(max_length=20, blank=True)
+	RefSeqNucleotide = models.CharField(max_length=15, blank=True, verbose_name="Nucleotide Sequence (RefSeq)")
+	RefSeqNucleotide_gi = models.IntegerField(max_length=10, blank=True)
+	WormBaseID = models.CharField(max_length=50, blank=True)
+	FlyBaseID = models.CharField(max_length=50, blank=True)
+	SGD_ID = models.CharField(max_length=25, blank=True)
+	public = models.BooleanField()
+	published = models.BooleanField()
+	def __unicode__(self):
+		return u'%s' % self.name
 	
