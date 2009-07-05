82e94dd0711a6c1b8ed97eeccb774ec8a7096646
Dave Bridges <dave.bridges@gmail.com>
2009-07-05 14:30:43 +0000

    added sgd mysql import details

--- datasets/sgd_phenotype.sql
+++ datasets/sgd_phenotype.sql
@@ -1,6 +1,12 @@
 create table via syncdb
 open mysql shell
 alter table datasets_sgd_phenotypes drop id;
-load data local infile 'phenotype_data.tab' into table datasets_sgd_phenotypes fields terminated by '\t' lines terminated by '\n' ;
+load data local infile 'H:\phenotype_data.tab' into table datasets_sgd_phenotypes fields terminated by '\t' lines terminated by '\n' ;
 alter table datasets_sgd_phenotypes add `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY;
+
+alter table datasets_sgd_interactions drop id;
+load data local infile 'H:\interaction_data.tab' into table datasets_sgd_interactions fields terminated by '\t' lines terminated by '\n' ;
+alter table datasets_sgd_interactions add `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY;
+
+load data local infile 'H:\registry.genenames.tab' into table datasets_sgd_genenames fields terminated by '\t' lines terminated by '\n' ;
 
