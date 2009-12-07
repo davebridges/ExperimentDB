d36662d10cf8fbe6161c04dcf41fcef01e74fca9
Dave Bridges <dave.bridges@gmail.com>
2009-12-07 02:57:09 +0000

    added documentation for data.views

--- Doc/api.rst
+++ Doc/api.rst
@@ -1,18 +1,35 @@
-Database Models
+Package Details
 ===============
 Data Package
 ------------
 
+Models
+++++++
 .. automodule:: data.models
    :members:
    :undoc-members:
    :inherited-members:
 
+Views
++++++
+.. automodule:: data.views
+   :members:
+   :undoc-members:
+   :inherited-members:
 
 Datasets Package
 ----------------
 
+Models
+++++++
 .. automodule:: datasets.models
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+Views
++++++
+.. automodule:: datasets.views
    :members:
    :undoc-members:
    :inherited-members:
@@ -21,16 +38,33 @@ Datasets Package
 Reagents Package
 ----------------
 
+Models
+++++++
 .. automodule:: reagents.models
    :members:
    :undoc-members:
    :inherited-members:
 
+Views
++++++
+.. automodule:: reagents.views
+   :members:
+   :undoc-members:
+   :inherited-members:
 
 Externals Package
 -----------------
 
+Models
+++++++
 .. automodule:: external.models
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+Views
++++++
+.. automodule:: external.views
    :members:
    :undoc-members:
    :inherited-members:
@@ -38,23 +72,48 @@ Externals Package
 Proteins Package
 ----------------
 
+Models
+++++++
 .. automodule:: proteins.models
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+Views
++++++
+.. automodule:: proteins.views
    :members:
    :undoc-members:
    :inherited-members:
 
 Projects Package
 ----------------
-
+Models
+++++++
 .. automodule:: projects.models
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+Views
++++++
+.. automodule:: projects.views
    :members:
    :undoc-members:
    :inherited-members:
 
 Cloning Package
 ----------------
-
+Models
+++++++
 .. automodule:: cloning.models
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+Views
++++++
+.. automodule:: cloning.views
    :members:
    :undoc-members:
    :inherited-members:
@@ -63,7 +122,16 @@ Cloning Package
 Construct Sharing Package
 -------------------------
 
+Models
+++++++
 .. automodule:: sharing.models
+   :members:
+   :undoc-members:
+   :inherited-members:
+
+Views
++++++
+.. automodule:: sharing.views
    :members:
    :undoc-members:
    :inherited-members:
--- data/views.py
+++ data/views.py
@@ -12,11 +12,19 @@ from django.http import HttpResponseRedirect
 
 @login_required
 def index(request):
+	"""This view shows a list of all experiments.
+
+	This list is ordered by the experiment date in descending order.  This view could potentially be rendered by a generic view.
+	"""
 	experiment_list = Experiment.objects.order_by('-experiment_date')
 	return render_to_response('experiment_list.html', {'experiment_list': experiment_list},context_instance=RequestContext(request))
 
 @login_required
 def experiment(request, experimentID):
+	"""This renders a detailed page of an experiment.
+
+	The view will show the experiment, and all associated reagents, proteins, projects and results associated with this object.
+	"""
 	experiment = get_object_or_404(Experiment, pk=experimentID)
 	exp_ID = experiment.experimentID
 	exp_result=Result.objects.filter(experiment__experimentID=exp_ID)
@@ -32,17 +40,29 @@ def experiment(request, experimentID):
 
 @login_required
 def protocol_list(request):
+	"""This renders a view in which all protocols are displayed.
+
+	In the case of deprecated protocols (i.e. protocols not marked as active), these are not shown.  This view could also be rendered as a generic view.
+	"""
 	protocol_list = Protocol.objects.all()
 	return render_to_response('protocol_list.html', {'protocol_list':protocol_list, },context_instance=RequestContext(request))
 
 @login_required
 def protocol_detail(request, protocol_slug):
+	"""This renders a view in which a protocol detail page is shown.
+
+	This view should be deprecated in favor of a redirection directly to the wiki page for this protocol
+	"""
 	protocol = get_object_or_404(Protocol, protocol_slug=protocol_slug)
 	protocol_experiments = Experiment.objects.filter(protocol=protocol)	
 	return render_to_response ('protocol_detail.html', {'protocol': protocol, 'protocol_experiments':protocol_experiments},context_instance=RequestContext(request))
 
 @login_required
 def result_new(request, experimentID):
+	"""This renders a form to add a new result.
+
+	This view will be sent from a particular experiment and will attach the result to that particular experiment.	
+	"""
 	experiment = get_object_or_404(Experiment, pk=experimentID)
 	if request.method == "POST":
 		form = ResultForm(request.POST, request.FILES)
