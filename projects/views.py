ef6d50985451f236f2cd514acf1099c7992e1426
Dave Bridges <dave.bridges@gmail.com>
2010-02-08 20:56:48 +0000

    fixed some redundancy in projects/views.py

--- projects/views.py
+++ projects/views.py
@@ -13,17 +13,12 @@ def index(request):
 @login_required
 def detail(request, project):
 	project = get_object_or_404(Project, pk=project)
-	projID=project.project_slug
-	proj_subproject=SubProject.objects.filter(project__project_slug=projID)
-	proj_experiment=Experiment.objects.filter(project__project_slug=projID)
-	return render_to_response('project_detail.html', {'project': project, 'proj_subproject':proj_subproject, 'proj_experiment': proj_experiment},context_instance=RequestContext(request))
+	return render_to_response('project_detail.html', {'project': project},context_instance=RequestContext(request))
 
 @login_required
 def subproject_detail(request, subproject):
 	subproject = get_object_or_404(SubProject, pk=subproject)
-	subprojID=subproject.project_slug
-	proj_experiment=Experiment.objects.filter(subproject__project_slug=subprojID)
-	return render_to_response('project_detail_sub.html', {'subproject': subproject, 'proj_experiment': proj_experiment},context_instance=RequestContext(request))
+	return render_to_response('project_detail_sub.html', {'subproject': subproject},context_instance=RequestContext(request))
 
 
 
