ebcc05c6db0e11643693e5b89ae064a451f97ed3
Dave Bridges <dave.bridges@gmail.com>
2010-04-21 14:10:01 +0000

    added named urls to proteins module.  Updated protein_index.html with corrected links

--- proteins/urls.py
+++ proteins/urls.py
@@ -4,32 +4,32 @@ from experimentdb.proteins.forms import ProteinForm
 from experimentdb.proteins.models import ProteinFamily, ProteinDetail
 
 urlpatterns = patterns('',
-	(r'^new/$', 'django.views.generic.create_update.create_object', {
+	url(r'^new/$', 'django.views.generic.create_update.create_object', {
 		'form_class': ProteinForm, 
 		'template_name': 'protein_form.html', 
 		'login_required':True ,
 		'post_save_redirect':"/experimentdb/protein"
-		}),
-	(r'^family/$', 'django.views.generic.list_detail.object_list', {
+		}, name='protein-new'),
+	url(r'^family/$', 'django.views.generic.list_detail.object_list', {
 		"queryset": ProteinFamily.objects.all(), 
 		'template_name': 'protein_family_list.html',
-		}),
-	(r'^family/new/$', 'django.views.generic.create_update.create_object', {
+		}, name='protein-family-list'),
+	url(r'^family/new/$', 'django.views.generic.create_update.create_object', {
 		'model': ProteinFamily, 
 		'template_name': 'protein_family_form.html', 
 		'login_required':True ,
 		'post_save_redirect': "/experimentdb/protein/family"
-		}),	
-	(r'^family/(?P<object_id>[\d]+)/$', 'django.views.generic.list_detail.object_detail', {
+		}, name='protein-family-new'),	
+	url(r'^family/(?P<object_id>[\d]+)/$', 'django.views.generic.list_detail.object_detail', {
 		"queryset": ProteinFamily.objects.all(), 
 		'template_name': 'protein_family_detail.html'
-		,}),
-	(r'^$', 'experimentdb.proteins.views.index'),
-	(r'^new/$', 'django.views.generic.create_update.create_object', {
+		,}, name='protein-family-detail'),
+	url(r'^$', 'experimentdb.proteins.views.index', name='protein-new'),
+	url(r'^new/$', 'django.views.generic.create_update.create_object', {
 		'model': ProteinDetail, 
 		'template_name': 'proteindetail_form.html', 
 		'login_required':True
-	}),
-	(r'^(?P<protein_id>[\d]+)/$', 'experimentdb.proteins.views.protein_isoform_detail'),
-	(r'^(?P<protein>[-\w\d]+)/$', 'experimentdb.proteins.views.detail'),	
+		}, name='protein-new'),
+	url(r'^(?P<protein_id>[\d]+)/$', 'experimentdb.proteins.views.protein_isoform_detail', name='protein-isoform-detail'),
+	url(r'^(?P<protein>[-\w\d]+)/$', 'experimentdb.proteins.views.detail', name='protein-detail'),	
 )
