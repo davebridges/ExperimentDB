cfaaec10d94a87faf2c7ebc2fa5a50f9b55f50c5
Dave Bridges <dave.bridges@gmail.com>
2010-05-23 18:31:03 +0000

    Updated to include CSRF middleware with Django 1.2  see http://docs.djangoproject.com/en/dev/ref/contrib/csrf/

--- settings.py
+++ settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware'
 )
 
 ROOT_URLCONF = 'experimentdb.urls'
