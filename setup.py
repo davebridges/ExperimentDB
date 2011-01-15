640f986165c9b3e7d137d4e76f1f6a75aa379ff6
Dave Bridges <dave.bridges@gmail.com>
2011-01-15 01:47:36 +0000

    updated documentation to 0.2.dev

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
 
-version = '0.2'
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+version = '0.2.dev'
 
 setup(name='experimentdb',
       version=version,
       description="A web based application for storage and organization of data regarding experimental data.",
-      long_description = read('README.rst'),
+      long_description = read('README'),
       classifiers=[        
         'Development Status :: 4 - Beta',
         'Framework :: Django',
@@ -18,12 +21,12 @@ setup(name='experimentdb',
         'Programming Language :: Python',
         'Intended Audience :: Science/Research',
         'Topic :: Database :: Front-Ends',
-        'Topic :: Scientific/Engineering :: Bio-Informatics'		
+        'Topic :: Scientific/Engineering :: Bio-Informatics',		
         'Topic :: Internet :: WWW/HTTP',], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='experiment, lims, data, science, data-management',
+      keywords='experiment, LIMS, data, science, data-management',
       author='Dave Bridges',
       author_email='dave.bridges@gmail.com',
-      url='http://davebridges.github.com',
+      url='http://code.google.com/p/experimentdb/',
       license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
