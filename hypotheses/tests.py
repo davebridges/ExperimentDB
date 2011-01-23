56b8a1a1f55d9888d1eaf32f876f2947fe00c09d
Dave Bridges <dave.bridges@gmail.com>
2011-01-23 16:29:27 +0000

    Added HypothesisModelTests

--- hypotheses/tests.py
+++ hypotheses/tests.py
@@ -8,14 +8,14 @@ from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
 
-from experimentdb.hypotheses.models import Hypothesis, Manipulation, Effect, Process
+from experimentdb.hypotheses.models import Hypothesis, Manipulation, Effect, Process, Context, Evidence
 
-MODELS = [Hypothesis, Manipulation, Effect, Process]
+MODELS = [Hypothesis, Manipulation, Effect, Process, Context, Evidence]
 
 class HypothesisModelTests(TestCase):
     """Tests the model attributes of Hypothesis objects contained in the hypotheses app."""
 
-    fixtures = ['test_manipulation','test_protein', 'test_process', 'test_entity']
+    fixtures = ['test_manipulation','test_protein', 'test_process', 'test_entity', 'test_context', 'test_evidence', 'test_hypothesis']
     
     def setUp(self):
         """Instantiate the test client.  Creates a test user."""
@@ -41,7 +41,37 @@ class HypothesisModelTests(TestCase):
         test_hypothesis.save()
         self.assertEquals(test_hypothesis.__unicode__(), "Fixture Protein Overexpression positively regulates glucose import")
 		
-    def test_create_hypothesis_create_date(self):
+    def test_create_hypothesis_all_fields_process(self):
+        """This is a test for creating a new Hypothesis object, with all fields being entered.  There is one test for process and one for entity (since both cannot be entered simultaneously).  It also tests the unicode representation."""
+        test_hypothesis = Hypothesis(
+		    manipulation = Manipulation.objects.get(pk=1), 
+			effect = Effect.objects.get(pk=1),
+			process = Process.objects.get(pk=1),
+			context = Context.objects.get(pk=1),
+			)
+        test_hypothesis.save()
+        test_evidence = Evidence.objects.get(pk=1)
+        test_hypothesis.evidence.add(test_evidence) # tests the addition of evidence as a m2m field
+        test_hypothesis.identical_hypotheses.add(test_hypothesis)	# tests the addition of a symmetrical hypothesis as a m2m field
+        self.assertEquals(test_hypothesis.__unicode__(), 'Fixture Protein Overexpression positively regulates glucose import in Fixture Context')
+
+def test_create_hypothesis_all_fields_entity(self):		
+        """This is a test for creating a new Hypothesis object, with all fields being entered.  There is one test for process and one for entity (since both cannot be entered simultaneously).  It also tests the unicode representation."""
+	
+        test_hypothesis = Hypothesis(
+		    manipulation = Manipulation.objects.get(pk=1), 
+			effect = Effect.objects.get(pk=1),
+			process = Process.objects.get(pk=1),
+			context = Context.objects.get(pk=1),
+			evidence = Evidence.objects.get(pk=1),
+			)
+        test_hypothesis.save()
+        test_evidence = Evidence.objects.get(pk=1)
+        test_hypothesis.evidence.add(test_evidence) # tests the addition of evidence as a m2m field
+        test_hypothesis.identical_hypotheses.add(test_hypothesis)	# tests the addition of a symmetrical hypothesis as a m2m field		
+        self.assertEquals(test_hypothesis.__unicode__(), 'Fixture Protein Overexpression positively regulates glucose import in Fixture Context')			
+		
+def test_create_hypothesis_create_date(self):
         """This is a test for checking that the create and modified date are being set correctly."""
         test_hypothesis = Hypothesis(
 		    manipulation = Manipulation.objects.get(pk=1), 
