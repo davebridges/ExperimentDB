eecbe8289720a4e411320cdfaa5500d3f4c4af92
Dave Bridges <dave.bridges@gmail.com>
2012-08-03 19:05:38 -0400

    Factored out URLS and SECRET_KEY from settings.py

--- experimentdb/settings.py
+++ experimentdb/settings.py
@@ -32,21 +32,6 @@ USE_I18N = True
 MEDIA_ROOT = os.path.join(PROJECT_DIR, "media/files")
 STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/static/admin-media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'ci%^08ig-0qu*&b(kz_=n6lvbx*puyx6=8!yxzm0+*z)w@7+%6'
-
-LOGIN_URL = '/experimentdb/accounts/login/'
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
