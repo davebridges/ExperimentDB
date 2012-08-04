c40eb9c813b70c270e57333d1ba0b931f5a1a058
Dave Bridges <dave.bridges@gmail.com>
2012-08-04 14:20:34 -0400

    Removed relative url redirects in cloning app.  Part of issue #1

--- cloning/urls.py
+++ cloning/urls.py
@@ -10,6 +10,7 @@ All views in this app start from a request of /experimentdb/cloning and direct t
 """
 
 from django.conf.urls.defaults import *
+from django.core.urlresolvers import reverse
 
 from cloning.models import Cloning, Mutagenesis
 from cloning import views
@@ -18,21 +19,18 @@ urlpatterns = patterns('',
 	url(r'^plasmid/new/$', 'django.views.generic.create_update.create_object', {
 		'model': Cloning, 
 		'template_name': 'cloning_form.html', 
-		'login_required':True ,
-		'post_save_redirect':"/experimentdb/cloning/plasmid"
+		'login_required':True 
 		}, name="cloning-new"),
 	url(r'^cloning/(?P<pk>\d+)/$', views.CloningDetail.as_view(), name='cloning_detail'),	
 	url(r'^mutagenesis/new/$', 'django.views.generic.create_update.create_object', {
 		'model': Mutagenesis, 
 		'template_name': 'mutagenesis_form.html', 
-		'login_required':True ,
-		'post_save_redirect':"/experimentdb/cloning/mutagenesis"
+		'login_required':True
 		}, name="mutagenesis-new"),
 	url(r'^mutagenesis/(?P<object_id>[\d]+)/edit/$', 'django.views.generic.create_update.update_object', {
 		'model': Mutagenesis, 
 		'template_name': 'mutagenesis_form.html', 
-		'login_required':True ,
-		'post_save_redirect':"/experimentdb/cloning/mutagenesis"
+		'login_required':True 
 		}, name="mutagenesis-edit"),
 	url(r'^mutagenesis/$', 'django.views.generic.list_detail.object_list', {
 		"queryset": Mutagenesis.objects.all(), 
