89217b054cd3f9fb0a9dbc241454bec74cf9c0f2
Dave Bridges <dave.bridges@gmail.com>
2012-08-06 17:43:45 -0400

    Removed species model and added absolute_url and docstring for Protein and ProteinIsoform classes

--- proteins/models.py
+++ proteins/models.py
@@ -14,44 +14,54 @@ class ProteinFamily(models.Model):
 		verbose_name_plural = "Protein Families"
 
 class Protein(models.Model):
-	protein_family = models.ManyToManyField('ProteinFamily')
-	name = models.CharField(max_length=25)
-	def __unicode__(self):
-		return u'%s' % self.name
-	@models.permalink
-	def get_absolute_url(self):
-		return ('protein-detail', [int(self.id)])
-	class Meta:
+    '''This object contains protein information.
+    
+    A Protein object is a generic link to all isoforms/splice variants and species of a protein.
+    The intent is so that a protein can be referred to generically in a species and isoform independent manner.
+    '''
+    protein_family = models.ManyToManyField('ProteinFamily', blank=True, null=True)
+    name = models.CharField(max_length=25)
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('protein-detail', [int(self.id)])
+
+    class Meta:
 		ordering = ['name']
 		verbose_name_plural = "Proteins"
 		
-class Species(models.Model):
-	common_name = models.CharField(max_length=50)
-	scientific_name = models.CharField(max_length=50, blank=True)
-	taxonomy_id = models.IntegerField(blank=True)
-	def __unicode__(self):
-		return '%s' % self.common_name
-	class Meta:
-		verbose_name_plural = "Species"
-	
 		
 class ProteinDetail(models.Model):
-	name = models.CharField(max_length=50, help_text="Our Name for the Protein")
-	protein = models.ForeignKey('Protein')
-	gene = models.CharField(max_length=10, blank=True)
-	species = models.ForeignKey('Species')
-	geneID = models.IntegerField(blank=True)
-	RefSeqProtein = models.CharField(max_length=15, blank=True, verbose_name="Protein Sequence (RefSeq)")
-	RefSeqProtein_gi = models.IntegerField(max_length=20, blank=True)
-	RefSeqNucleotide = models.CharField(max_length=15, blank=True, verbose_name="Nucleotide Sequence (RefSeq)")
-	RefSeqNucleotide_gi = models.IntegerField(max_length=10, blank=True)
-	WormBaseID = models.CharField(max_length=50, blank=True)
-	FlyBaseID = models.CharField(max_length=50, blank=True)
-	SGD_ID = models.CharField(max_length=25, blank=True)
-	public = models.BooleanField()
-	published = models.BooleanField()
-	def __unicode__(self):
-		return u'%s' % self.name
-	class Meta:
+    '''The protein detail contains isoform and species level detail of a protein.
+    
+    Since a Protein object is a generic link to all isoforms/splice variants and species of a protein this allows for links to the specific protein details when needed.'''
+    
+    name = models.CharField(max_length=50, help_text="Our Name for the Protein")
+    protein = models.ForeignKey('Protein')
+    gene = models.CharField(max_length=10, blank=True)
+    species = models.ForeignKey('reagents.Species', blank=True, null=True)
+    geneID = models.IntegerField(blank=True, null=True)
+    RefSeqProtein = models.CharField(max_length=15, blank=True, null=True, verbose_name="Protein Sequence (RefSeq)")
+    RefSeqProtein_gi = models.IntegerField(max_length=20, blank=True, null=True)
+    RefSeqNucleotide = models.CharField(max_length=15, blank=True, null=True, verbose_name="Nucleotide Sequence (RefSeq)")
+    RefSeqNucleotide_gi = models.IntegerField(max_length=10, blank=True, null=True)
+    WormBaseID = models.CharField(max_length=50, blank=True, null=True)
+    FlyBaseID = models.CharField(max_length=50, blank=True, null=True)
+    SGD_ID = models.CharField(max_length=25, blank=True, null=True)
+    public = models.BooleanField()
+    published = models.BooleanField()
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        '''The permalink for a protein detail is regulated by the protein-isoform-detail link.'''
+        return ('protein-isoform-detail', [int(self.id)])
+        
+    class Meta:
 		verbose_name_plural = "Protein Isoforms"
 	
