1960d8e75bdbaf54083a0995e765436516ded088
Dave Bridges <dave.bridges@gmail.com>
2012-08-06 17:42:15 -0400

    Removed nonfunctional SelectWithPop code.

--- proteins/forms.py
+++ proteins/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 
 from proteins.models import Protein, ProteinFamily
-from experimentdb.widgets import SelectWithPop
-
 
 class ProteinForm(forms.ModelForm):
-	protein_family = forms.ModelChoiceField(ProteinFamily.objects, widget=SelectWithPop)
 	class Meta:
 		model = Protein
 		
