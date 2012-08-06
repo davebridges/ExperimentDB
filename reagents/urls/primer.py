087e2eb45a649c312210cece49bf9d096637e873
Dave Bridges <dave.bridges@gmail.com>
2012-08-06 12:05:12 -0400

    Removed relative urls from reagents app and switched to named urls.  Part of issue #1.

--- reagents/urls/primer.py
+++ reagents/urls/primer.py
@@ -47,7 +47,6 @@ urlpatterns = patterns('',
 	url(r'^(?P<object_id>[\d]+)/delete$', delete_primer, {
 		'model': Primer, 
 		'login_required':True,
-		'template_name': 'confirm_delete.html',
-		'post_delete_redirect': '/experimentdb/primer'
+		'template_name': 'confirm_delete.html'
 		,}, name="primer-delete"),
 )
