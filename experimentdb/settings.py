6057ace6e48579358359e4c1c8dda7bdd466442c
Dave Bridges <dave.bridges@gmail.com>
2012-09-09 16:49:42 -0400

    Updated template loaders setting.  Part of issue #19.

--- experimentdb/settings.py
+++ experimentdb/settings.py
@@ -27,11 +27,8 @@ USE_TZ = True
 USE_I18N = True
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
-)
+TEMPLATE_LOADERS = ('django.template.loaders.filesystem.Loader',
+ 'django.template.loaders.app_directories.Loader')
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
