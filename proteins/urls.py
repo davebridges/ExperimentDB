067e378c9c61f27fb1cb98b9d53f7035922b065a
Dave Bridges <dave.bridges@gmail.com>
2013-06-08 21:42:22 -0500

    Created new CRUD views for Protein and ProteinFamily objects.

--- proteins/urls.py
+++ proteins/urls.py
@@ -19,7 +19,19 @@ This app takes a url request in the form of /protein/something and redirects to 
 
 from django.conf.urls.defaults import *
 
+from proteins import views
 
 urlpatterns = patterns('',			
 	url(r'^(?P<protein_id>[\d]+)/$', 'proteins.views.protein_isoform_detail', name='protein-isoform-detail'),
-	url(r'^(?P<protein>[-\w\d]+)/$', 'proteins.views.detail', name='protein-name-slug'))
+	url(r'^(?P<protein>[-\w\d]+)/$', 'proteins.views.detail', name='protein-name-slug'),
+	url(r'^/new/$', views.ProteinCreate.as_view(), name="protein-new"),
+    url(r'^/(?P<pk>\d+)/$', views.ProteinDetail.as_view(), name="protein-detail"),
+	url(r'^/(?P<pk>\d+)/edit$', views.ProteinUpdate.as_view(), name="protein-edit"),
+	url(r'^/(?P<pk>\d+)/delete$', views.ProteinDelete.as_view(), name="protein-delete"),
+    url(r'^/$', views.ProteinList.as_view(), name="protein-list"),
+	url(r'^protein-family/new/$', views.ProteinFamilyCreate.as_view(), name="protein-family-new"),
+    url(r'^protein-family/(?P<pk>\d+)/$', views.ProteinFamilyDetail.as_view(), name="protein-family-detail"),
+	url(r'^protein-family/(?P<pk>\d+)/edit$', views.ProteinFamilyUpdate.as_view(), name="protein-family-edit"),
+	url(r'^protein-family/(?P<pk>\d+)/delete$', views.ProteinFamilyDelete.as_view(), name="protein-family-delete"),
+    url(r'^protein-family/$', views.ProteinFamilyList.as_view(), name="protein-family-list")
+)
--- proteins/views.py
+++ proteins/views.py
@@ -1,13 +1,20 @@
+
+'''This package controls the views generated for the proteins app'''
+
+
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, Http404
 from reagents.models import Antibody, Construct, Primer
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
+
+from braces.views import LoginRequiredMixin, PermissionRequiredMixin
 
 from Bio import Entrez
 from Bio import SeqIO
 
-from proteins.models import Protein
+from proteins.models import Protein, ProteinFamily
 from proteins.forms import ProteinFamilyForm
 from data.models import Experiment
 
@@ -42,5 +49,80 @@ def protein_isoform_detail(request, protein_id):
 @login_required
 def newProteinFamily(request):
 	return handlePopAdd(request, ProteinFamilyForm, 'protein_family')
+    
+class ProteinCreate(PermissionRequiredMixin, CreateView):
+    '''This view is for creating a new Protein.'''
+    
+    model = Protein
+    template_name = 'protein_form.html'
+    permission_required = "proteins.create_protein"
 
+class ProteinDetail(LoginRequiredMixin, DetailView):
+    '''This view is for viewing Protein.'''
+    
+    model = Protein
+    template_name = 'protein_detail.html'
+    template_object_name = 'protein'
+
+    
+class ProteinUpdate(PermissionRequiredMixin, UpdateView):
+    '''This view is for editing a Protein.'''
+    
+    model = Protein
+    template_name = 'protein_form.html'
+    template_object_name = 'protein'
+    permission_required = "proteins.update_protein"    
+    
+class ProteinDelete(PermissionRequiredMixin, DeleteView):
+    '''This view is for deleting a Protein.'''
+    
+    model = Protein
+    template_name = 'confirm_delete.html'
+    template_object_name = 'object'
+    permission_required = "proteins.delete_protein"              
+    
+class ProteinList(LoginRequiredMixin, ListView):
+    '''This view is for viewing a list of Protein.'''
+    
+    model = Protein
+    template_name = 'protein_list.html'
+    template_object_name = 'protein_list' 
+    
+class ProteinFamilyCreate(PermissionRequiredMixin, CreateView):
+    '''This view is for creating a new ProteinFamily.'''
+    
+    model = ProteinFamily
+    template_name = 'proteinfamily_form.html'
+    permission_required = "proteins.create_proteinfamily"
+
+class ProteinFamilyDetail(LoginRequiredMixin, DetailView):
+    '''This view is for viewing ProteinFamily.'''
+    
+    model = ProteinFamily
+    template_name = 'proteinfamily_detail.html'
+    template_object_name = 'proteinfamily'
+
+    
+class ProteinFamilyUpdate(PermissionRequiredMixin, UpdateView):
+    '''This view is for editing a ProteinFamily.'''
+    
+    model = ProteinFamily
+    template_name = 'proteinfamily_form.html'
+    template_object_name = 'proteinfamily'
+    permission_required = "proteins.update_proteinfamily"    
+    
+class ProteinFamilyDelete(PermissionRequiredMixin, DeleteView):
+    '''This view is for deleting a ProteinFamily.'''
+    
+    model = ProteinFamily
+    template_name = 'confirm_delete.html'
+    template_object_name = 'object'
+    permission_required = "proteins.delete_proteinfamily"              
+    
+class ProteinFamilyList(LoginRequiredMixin, ListView):
+    '''This view is for viewing a list of ProteinFamily.'''
+    
+    model = ProteinFamily
+    template_name = 'proteinfamily_list.html'
+    template_object_name = 'proteinfamily_list'         
 
