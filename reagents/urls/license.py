190af6c83bcfe4e91e12c666bba02e98faff9bcc
Dave Bridges <dave.bridges@gmail.com>
2013-06-08 22:26:35 -0500

    Fixed url transitions (added quotes around links).  Part of issue #21

--- reagents/urls/license.py
+++ reagents/urls/license.py
@@ -6,9 +6,9 @@ from reagents import views
 
 urlpatterns = patterns('',
 	url(r'^new/?$', views.LicenseCreate.as_view(), name="license-new"),
-    url(r'^(?P<slug>[\w-]+)/?$', views.LicenseDetail.as_view(), name="license-detail"),
-	url(r'^(?P<slug>[\w-]+)/edit/?$', views.LicenseUpdate.as_view(), name="license-edit"),
-	url(r'^(?P<slug>[\w-]+)/delete/?$', views.LicenseDelete.as_view(), name="license-delete"),
+    url(r'^(?P<pk>\d+)/?$', views.LicenseDetail.as_view(), name="license-detail"),
+	url(r'^(?P<pk>\d+)/edit/?$', views.LicenseUpdate.as_view(), name="license-edit"),
+	url(r'^(?P<pk>\d+)/delete/?$', views.LicenseDelete.as_view(), name="license-delete"),
     url(r'^/?$', views.LicenseList.as_view(), name="license-list"),
 )
 
