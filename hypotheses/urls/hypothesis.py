75dead1b84a4a1f73718cae9e012d104aaac203f
Dave Bridges <dave.bridges@gmail.com>
2013-08-16 10:27:46 -0500

    Altered urlconf to indicate pk rather than slug

--- hypotheses/urls/hypothesis.py
+++ hypotheses/urls/hypothesis.py
@@ -6,9 +6,9 @@ from hypotheses import views
 
 urlpatterns = patterns('',
 	url(r'^new/$', views.HypothesisCreate.as_view(), name="hypothesis-new"),
-    url(r'^(?P<slug>[\w-]+)/$', views.HypothesisDetail.as_view(), name="hypothesis-detail"),
-	url(r'^(?P<slug>[\w-]+)/edit$', views.HypothesisUpdate.as_view(), name="hypothesis-edit"),
-	url(r'^(?P<slug>[\w-]+)/delete$', views.HypothesisDelete.as_view(), name="hypothesis-delete"),
-    url(r'^$', views.HypothesisList.as_view(), name="hypothesis-list"),
+        url(r'^(?P<pk>[\d]+)/$', views.HypothesisDetail.as_view(), name="hypothesis-detail"),
+	url(r'^(?P<pk>[\d]+)/edit$', views.HypothesisUpdate.as_view(), name="hypothesis-edit"),
+	url(r'^(?P<pk>[\d]+)/delete$', views.HypothesisDelete.as_view(), name="hypothesis-delete"),
+        url(r'^$', views.HypothesisList.as_view(), name="hypothesis-list"),
 )
 
