b121548a32cace89c1d8d2936a2c811899fbf615
Dave Bridges <dave.bridges@gmail.com>
2013-10-17 15:06:05 -0500

    Fixed url issue for primers.  Closes #25

--- reagents/urls/primer.py
+++ reagents/urls/primer.py
@@ -6,9 +6,9 @@ from reagents import views
 
 urlpatterns = patterns('',
 	url(r'^new/$', views.PrimerCreate.as_view(), name="primer-new"),
-    url(r'^(?P<slug>[\w-]+)/$', views.PrimerDetail.as_view(), name="primer-detail"),
-	url(r'^(?P<slug>[\w-]+)/edit$', views.PrimerUpdate.as_view(), name="primer-edit"),
-	url(r'^(?P<slug>[\w-]+)/delete$', views.PrimerDelete.as_view(), name="primer-delete"),
+        url(r'^(?P<pk>[\d-]+)/$', views.PrimerDetail.as_view(), name="primer-detail"),
+	url(r'^(?P<pk>[\d-]+)/edit$', views.PrimerUpdate.as_view(), name="primer-edit"),
+	url(r'^(?P<pk>[\d-]+)/delete$', views.PrimerDelete.as_view(), name="primer-delete"),
     url(r'^$', views.PrimerList.as_view(), name="primer-list"),
 )
 
