6727dc28818d73350d41e9c1ea83bd1b0b912be9
Dave Bridges <dave.bridges@gmail.com>
2014-03-11 11:00:11 -0500

    Fixed url errors in construct pages.  Closes issue #8

--- reagents/urls/construct.py
+++ reagents/urls/construct.py
@@ -6,9 +6,9 @@ from reagents import views
 
 urlpatterns = patterns('',
 	url(r'^new/$', views.ConstructCreate.as_view(), name="construct-new"),
-    url(r'^(?P<slug>[\w-]+)/$', views.ConstructDetail.as_view(), name="construct-detail"),
-	url(r'^(?P<slug>[\w-]+)/edit$', views.ConstructUpdate.as_view(), name="construct-edit"),
-	url(r'^(?P<slug>[\w-]+)/delete$', views.ConstructDelete.as_view(), name="construct-delete"),
-    url(r'^$', views.ConstructList.as_view(), name="construct-list"),
+        url(r'^(?P<pk>[\d-]+)/$', views.ConstructDetail.as_view(), name="construct-detail"),
+	url(r'^(?P<pk>[\d-]+)/edit$', views.ConstructUpdate.as_view(), name="construct-edit"),
+	url(r'^(?P<pk>[\d-]+)/delete$', views.ConstructDelete.as_view(), name="construct-delete"),
+        url(r'^$', views.ConstructList.as_view(), name="construct-list"),
 )
 
